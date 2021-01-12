95dce4316c78fd117a9d04a73f60c85a777b41cd
rachael-codes <mcbride.rac@gmail.com>
2021-01-12 13:43:10 -0500

    Add small problem

---
+++ small_problems/friday_13th?.rb
@@ -0,0 +1,22 @@
+# Watch Others Code: Video 4
+# Program: Friday the 13th?
+# Date: 01/12/21
+
+# Program: Write a method that returns the number of Friday the 13ths in a year passed in as an argument.
+
+require 'date'
+
+def friday_13th?(year)
+	friday_13th_count = 0
+	month = 1
+	
+	12.times do 
+		if Date.new(year, month, 13).friday?
+			friday_13th_count += 1
+		end 
+		month += 1
+	end 
+	friday_13th_count
+end 
+
+p friday_13th?(2002)
