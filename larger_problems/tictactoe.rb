1dd0744e91564102fe6e7a32c4182fcd1ac85b14
rachael-codes <mcbride.rac@gmail.com>
2021-02-02 16:22:11 -0500

    Add 21 Game Program

--- larger_problems/tictactoe.rb
+++ larger_problems/tictactoe.rb
@@ -1,110 +1,275 @@
 # Program: Tic Tac Toe Game
-# Date: 01/10/21
+# Date: 01/12/21
 
-require 'pry' 
+require 'pry'
 
+# CONSTANTS
 INITIAL_MARKER = ' '
 PLAYER_MARKER = 'X'
 COMPUTER_MARKER = 'O'
+MIDDLE_SQUARE = 5
 
+WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows 
+								[[2, 5, 8], [1, 4, 7], [3, 6, 9]] + # columns
+								[[1, 5, 9], [3, 5, 7]]              # diagonals
+
+# METHODS 
 def prompt(msg)
 	puts "=> #{msg}"
+end
+
+def clear_screen
+	system('clear')
+	system('cls')
 end 
 
-def display_board(brd)
-	system 'clear'
-	puts "You're a #{PLAYER_MARKER}. Computer is a #{COMPUTER_MARKER}."
-	puts ""
-	puts "     |     |"
-	puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
-	puts "     |     |"
-	puts "-----+-----+-----"
-	puts "     |     |"
-	puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}"
-	puts "     |     |"
-	puts "-----+-----+-----"
-	puts "     |     |"
-	puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}"	
-	puts "     |     |"
-end
+def welcome_user
+	clear_screen
+	prompt "Welcome to Tic Tac Toe!"
+	prompt "The first player to win five rounds is the winner."
+end 
 
 def initialize_board
 	new_board = {}
 	(1..9).each { |num| new_board[num] = INITIAL_MARKER }
 	new_board 
+end
+
+# rubocop: disable Metrics/MethodLength, Metrics/AbcSize
+def display_board(board)
+	puts "You're an #{PLAYER_MARKER}. Computer is a #{COMPUTER_MARKER}."
+	puts ""
+	puts "     |     |"
+	puts "  #{board[1]}  |  #{board[2]}  |  #{board[3]}"
+	puts "     |     |"
+	puts "-----+-----+-----"
+	puts "     |     |"
+	puts "  #{board[4]}  |  #{board[5]}  |  #{board[6]}"
+	puts "     |     |"
+	puts "-----+-----+-----"
+	puts "     |     |"
+	puts "  #{board[7]}  |  #{board[8]}  |  #{board[9]}"	
+	puts "     |     |"
+end
+# rubocop: enable Metrics/MethodLength, Metrics/AbcSize
+
+def initialize_score 
+	{ player: 0, computer: 0 }
+end
+
+def joinor(arr, punc =', ', conj = 'or ')
+	count = 1
+	stopper = (arr.size * 2 - 1)
+
+	if arr.size == 1
+		arr[0]
+	elsif arr.size == 2
+		arr.insert(1, " #{conj}")
+	else 
+		until count == stopper
+			arr.insert(count, punc)
+			count += 2
+		end 
+		arr.insert(-2, conj)
+	end
+	arr.join("")
 end 
 
-def empty_squares(brd)
-	brd.keys.select { |num| brd[num] == INITIAL_MARKER }
+def empty_squares(board)
+	board.keys.select { |num| board[num] == INITIAL_MARKER }
 end 
 
-def player_places_piece!(brd)
-	square = ''
+def select_user_square(empty_squares)
+	prompt "Choose a square #{joinor(empty_squares)}:"
+	square_number = 0
 	loop do 
-		prompt "Choose a square (#{empty_squares(brd).join(", ")}): "
-		square = gets.chomp.to_i
-		break if empty_squares(brd).include?(square)
-		prompt "Sorry, that's not a valid choice."
+		square_number = gets.chomp.to_i
+		break if valid_user_choice?(square_number, empty_squares)
+		prompt "That's not a valid choice. Try again."
 	end 
-	brd[square] = PLAYER_MARKER
+	square_number
 end 
 
-def computer_places_piece!(brd)
-	square = empty_squares(brd).sample
-	brd[square] = COMPUTER_MARKER
+def valid_user_choice?(choice, empty_squares)
+	empty_squares.include?(choice)
 end 
 
-def board_full?(brd)
-	empty_squares(brd).empty?
+def select_computer_square(allowed_squares, board)
+	defensive = detect_threatened_square(board, PLAYER_MARKER)
+	offensive = detect_threatened_square(board, COMPUTER_MARKER)
+	if offensive
+		offensive
+	elsif defensive
+		defensive
+	elsif allowed_squares.include?(MIDDLE_SQUARE)
+		MIDDLE_SQUARE
+	else 
+		allowed_squares.sample
+	end
+end
+
+def user_places_piece!(board, number)
+	board[number] = PLAYER_MARKER
 end 
 
-def someone_won?(brd)
-	!!detect_winner(brd)
+def computer_places_piece!(board, number)
+	prompt("The computer's move is...")
+	sleep(1)
+	board[number] = COMPUTER_MARKER
 end 
 
-def detect_winner(brd)
-	winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows 
-									[[2, 5, 8], [1, 4, 7], [3, 6, 9]] + # columns
-									[[1, 5, 9], [3, 5, 7]]              # diagonals
-	winning_lines.each do |line|
-		if brd[line[0]] == PLAYER_MARKER && 
-			 brd[line[1]] == PLAYER_MARKER && 
-			 brd[line[2]] == PLAYER_MARKER
-			 return 'Player'
-		elsif brd[line[0]] == COMPUTER_MARKER && 
-			 brd[line[1]] == COMPUTER_MARKER && 
-			 brd[line[2]] == COMPUTER_MARKER
-			 return 'Computer'
+def place_piece!(board, current_player)
+	if current_player == 'player'
+		user_places_piece!(board, select_user_square(empty_squares(board), board))
+	else 
+		computer_places_piece!(board, select_computer_square(empty_squares(board), board))
+	end
+end 
+
+def detect_threatened_square(board, marker)
+	WINNING_LINES.each do |line|
+		marked, threatened = line.partition do |num|
+			board[num] == marker
+		end 
+		if marked.size == 2 && board[threatened[0]] == INITIAL_MARKER
+      return threatened[0]
 		end 
 	end 
 	nil 
-end 
-
-loop do 
-	board = initialize_board
-	display_board(board)
-
-	loop do 
-		display_board(board)
-
-		player_places_piece!(board)
-		break if someone_won?(board) || board_full?(board) 
-
-		computer_places_piece!(board)
-		break if someone_won?(board) || board_full?(board) 
-	end
-
-	display_board(board)
-
-	if someone_won?(board)
-		prompt "#{detect_winner(board)} won!"
-	else 
-		prompt "It's a tie!"
-	end
-
-	prompt "Play again (y or n)?"
-	answer = gets.chomp
-	break unless answer.downcase.start_with?('y')
 end
 
+def user_won?(board)
+	WINNING_LINES.any? do |line|
+		line.all? do |num|
+			board[num] == PLAYER_MARKER
+		end
+	end
+end
+
+def computer_won?(board)
+	WINNING_LINES.any? do |line|
+		line.all? do |num|
+			board[num] == COMPUTER_MARKER
+		end
+	end
+end 
+
+def tie?(empty_squares)
+	empty_squares.empty?
+end
+
+def play_again?
+	prompt "Do you want to play again (yes/no)?"
+	answer = ''
+	loop do 
+		answer = gets.chomp 
+		break if %w(y yes n no).include?(answer.downcase)
+		prompt "Invalid response."
+	end 
+	%w(y yes).include?(answer.downcase)
+end
+
+def round_over?(board, empty_squares)
+	user_won?(board) || computer_won?(board) || tie?(empty_squares)
+end
+
+def display_wins_and_losses(scores)
+	prompt "You have won #{scores[:player]} times."
+	prompt "The computer has won #{scores[:computer]} times."
+end 
+
+def increment_user_score(scores)
+	scores[:player] += 1
+end 
+
+def increment_computer_score(scores)
+	scores[:computer] += 1
+end 
+
+def five_rounds_finished?(scores)
+	scores[:player] == 5 || scores[:computer] == 5
+end 
+
+def choose_current_player
+	prompt "Who do you want to go first? (Type m for 'me' or c for 'computer'.)"
+	first_mover = ''
+	loop do 
+		first_mover = gets.chomp.downcase
+		break if first_mover == 'm' || first_mover == 'c'
+		prompt "That's an invalid choice." 
+		prompt "Type m if you want to go first or c if you want the computer to go first."
+	end 
+	first_mover
+end
+
+def set_current_player(first_mover)
+	if first_mover == 'm' 
+		'player'
+	elsif first_mover == 'c'
+		'computer'
+	else 
+		choose_current_player
+	end 
+end 
+
+def alternate_player(current_player)
+	current_player == 'player' ? 'computer' : 'player'
+end
+
+def result_of_each_round(board, scores)
+	if user_won?(board)
+		prompt "You won this round!"
+		increment_user_score(scores)
+		sleep(1)
+	elsif computer_won?(board)
+		prompt "The computer won this round."
+		increment_computer_score(scores)
+		sleep(1)
+	else 
+		prompt "This round was a tie."
+		sleep(1)
+	end
+end 
+
+def display_full_game_winner(scores)
+	if scores[:player] == 5
+		prompt "You are the winner!"
+	else
+		prompt "It looks like the computer won this time."
+	end 
+end
+
+# MAIN PROGRAM 
+welcome_user 
+
+loop do 
+	scores = initialize_score
+
+	loop do
+		sleep(2)
+		clear_screen
+		board = initialize_board 
+		first_mover = choose_current_player
+		current_player = set_current_player(first_mover)
+
+		loop do 
+			display_board(board)
+			place_piece!(board, current_player)
+			current_player = alternate_player(current_player)
+			display_board(board, scores)
+			break if round_over?(board, empty_squares(board))
+		end 
+
+		result_of_each_round(board, scores)
+		display_board(board, scores)
+		break if five_rounds_finished?(scores)
+		prompt "Let's go to the next round..."
+	end 
+
+	display_wins_and_losses(scores)
+	display_full_game_winner(scores)
+	break unless play_again? 
+end
+
+clear_screen
 prompt "Thanks for playing Tic Tac Toe. Goodbye!"
---
+++ larger_problems/twenty_one.rb
@@ -0,0 +1,252 @@
+# Program: Twenty-One
+# Date: 2/2/21
+# Instructions: Create a game like the casino game 21. The best of three wins.
+
+CARDS = %w(2 3 4 5 6 7 8 9 10 Jack Queen King Ace 
+           2 3 4 5 6 7 8 9 10 Jack Queen King Ace 
+           2 3 4 5 6 7 8 9 10 Jack Queen King Ace 
+           2 3 4 5 6 7 8 9 10 Jack Queen King Ace).freeze 
+
+# METHODS
+def prompt(msg)
+  puts "=> #{msg}"
+end
+
+def clear_screen
+  system('clear')
+  system('cls')
+end 
+
+def welcome_user
+  clear_screen
+  prompt "Welcome to 21!"
+  prompt "Your goal is to try to get as close to 21 as possible without going over."
+  prompt "If you go over 21, it's a 'bust' and you lose."
+  prompt "The player who wins the best of three is the winner."
+  sleep(7)
+  clear_screen
+  prompt "To start, the dealer will deal two cards to you and two to itself." 
+  sleep(4)
+  clear_screen
+end 
+
+def start_new_deck
+  deck = CARDS.dup.shuffle
+  deck
+end 
+
+def deal_first_two_cards(deck, player_hand, dealer_hand)
+  player_hand << deck.shift(2)
+  dealer_hand << deck.shift(2)
+end 
+
+def deal_one_card(deck, player_or_dealer_hand)
+  player_or_dealer_hand << deck.shift(1)
+end 
+
+def convert_ace(total)
+  total > 21 ? total - 10 : total
+end 
+
+def calculate_total(player_or_dealer_hand)
+  hand = player_or_dealer_hand.flatten
+  result = hand.map do |card| 
+    if card == 'Jack' || card == 'Queen' || card == 'King'
+      10 
+    elsif card == 'Ace'
+      11
+    else 
+      card.to_i 
+    end 
+  end 
+  # account for possibility of ace 
+  result.include?(11) ? convert_ace(result.inject(:+)) : result.inject(:+)
+end 
+
+def display_player_hand(player_hand, player_total)
+  hand = player_hand.flatten
+  card1 = hand[0]
+  card2 = hand[-1]
+
+  if (card1 == 'Ace' || card1 == '8') && (card2 == 'Ace' || card2 == '8')
+    prompt "You were dealt an #{card1} and an #{card2}." 
+  elsif (card1 == 'Ace' || card1 == '8') && (card2 != 'Ace' && card2 != '8')
+    prompt "You were dealt an #{card1} and a #{card2}." 
+  elsif (card1 != 'Ace' && card1 != '8') && (card2 == 'Ace' || card2 == '8')
+    prompt "You were dealt a #{card1} and an #{card2}." 
+  else 
+    prompt "You were dealt a #{card1} and a #{card2}." 
+  end 
+
+  sleep(1)
+  prompt "Your total is #{player_total}."
+  puts
+  sleep(2)
+end 
+
+def display_dealer_hand(dealer_hand)
+  hand = dealer_hand.flatten
+  card1 = hand[0]
+  if card1 == 'Ace' || card1 == '8'
+    prompt "The dealer was dealt an #{card1} and an additional card." 
+  else 
+    prompt "The dealer was dealt a #{card1} and an additional card." 
+  end 
+  sleep(1)
+end 
+
+def player_takes_turn(deck, player_hand, initial_player_total)
+  player_total = initial_player_total
+  loop do 
+    sleep(1)
+    prompt "Do you want to hit or stay?"
+    puts
+    hit_or_stay = gets.chomp.downcase 
+    if hit_or_stay == 's' || hit_or_stay == 'stay'
+      break 
+    else 
+      deal_one_card(deck, player_hand)
+      new_hand = player_hand.flatten
+      player_total = calculate_total(new_hand)
+      puts 
+      prompt "You were dealt a #{new_hand[-1]}. Your total is now #{player_total}."
+      if player_total > 21 
+        prompt "Oh no. You busted!"
+        break 
+      end 
+    end 
+  end 
+  player_total
+end 
+
+def dealer_takes_turn(deck, dealer_hand, initial_dealer_total)
+  dealer_total = initial_dealer_total
+  loop do 
+  	sleep(2)
+  	puts
+    if dealer_total > 21 
+      prompt "The dealer busted!"
+      break 
+    elsif dealer_total >= 17 
+      prompt "The dealer has chosen to stay."
+      break 
+    else 
+      prompt "The dealer has drawn another card."
+      deal_one_card(deck, dealer_hand)
+      new_hand = dealer_hand.flatten
+      dealer_total = calculate_total(new_hand)
+    end 
+  end 
+  dealer_total
+end 
+
+def display_final_round_score(final_player_total, final_dealer_total)
+  sleep(2)
+  puts
+  prompt "Your final hand totaled to #{final_player_total}."
+  prompt "The dealer's final hand totaled to #{final_dealer_total}."
+end 
+
+def get_round_winner(final_player_total = 0, final_dealer_total = 0)
+  if (final_player_total < final_dealer_total) && (final_dealer_total <= 21) 
+    'dealer'
+  elsif 
+  	final_player_total == final_dealer_total
+  	nil
+  else
+    'player'
+  end 
+end 
+
+def display_round_winner(final_player_total, final_dealer_total)
+	sleep(2)
+	puts 
+  if final_player_total == final_dealer_total
+    prompt "It's a tie!"
+  elsif (final_player_total < final_dealer_total) && (final_dealer_total <= 21) 
+    prompt "Looks like the dealer won this time."
+  else 
+    prompt "Congratulations! It looks like you won."
+  end 
+end 
+
+def play_again?
+  sleep(2)
+  answer = nil 
+  loop do 
+    puts 
+    prompt "Would you like to play again ('y' for yes or 'n' for no)?"
+    answer = gets.chomp
+    break if answer.downcase.include?('y') || answer.downcase.include?('n')
+    prompt "That's not a valid choice."
+  end 
+  answer.downcase 
+end 
+
+def determine_ultimate_winner(player_wins, dealer_wins)
+	player_wins > dealer_wins ? "player" : "dealer"
+end 
+
+def display_result_of_multiple_rounds(player_wins, dealer_wins)
+	prompt "You won #{player_wins} times. The dealer won #{dealer_wins} times."
+	if determine_ultimate_winner(player_wins, dealer_wins) == 'player'
+		prompt "You are the winner!"
+	else 
+		prompt "Better luck next time!"
+	end 
+end 
+
+def display_final_msg(player_wins, dealer_wins)
+	puts
+	display_result_of_multiple_rounds(player_wins, dealer_wins)
+	sleep(7)
+	clear_screen
+	prompt "Thanks for playing 21!"
+	sleep(4)
+	clear_screen
+end 
+
+# MAIN GAME PLAY 
+welcome_user 
+round_count = 1
+player_wins = 0
+dealer_wins = 0
+
+loop do 
+	prompt "Round #{round_count}"
+	puts
+
+  player_hand = []
+  dealer_hand = []
+  deck = start_new_deck
+  deal_first_two_cards(deck, player_hand, dealer_hand)
+  initial_player_total = calculate_total(player_hand)
+  initial_dealer_total = calculate_total(dealer_hand)
+  display_player_hand(player_hand, initial_player_total)
+  display_dealer_hand(dealer_hand)
+  final_player_total = player_takes_turn(deck, player_hand, initial_player_total)
+
+  if final_player_total > 21 
+    sleep(2)
+    puts 
+    prompt "Since you busted, the dealer has won this round." 
+    winner = 'dealer'
+  else 
+    final_dealer_total = dealer_takes_turn(deck, dealer_hand, initial_dealer_total)
+    display_final_round_score(final_player_total, final_dealer_total)
+    winner = get_round_winner(final_player_total, final_dealer_total)
+    display_round_winner(final_player_total, final_dealer_total)
+  end 
+
+  dealer_wins += 1 if winner == 'dealer'
+  player_wins += 1 if winner == 'player'
+  break if player_wins == 3 || dealer_wins == 3
+
+  yes_or_no = play_again? 
+  break if yes_or_no.include?('n')
+  clear_screen
+
+  round_count += 1 
+end 
+
+display_final_msg(player_wins, dealer_wins)
